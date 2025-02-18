cb958bdbf6fbfe2e825a931c646c5a5ff7073db2
sagetechpdx <info@sagetech.support>
2025-02-18 12:43:01 -0800

    aaa

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,28 +1,84 @@
-import Link from 'next/link'
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
-import { Container } from '@/components/Container'
-import { FadeIn } from '@/components/FadeIn'
 
-export default function NotFound() {
+export default function Example() {
   return (
-    <Container className="flex h-full items-center pt-24 sm:pt-32 lg:pt-40">
-      <FadeIn className="flex max-w-xl flex-col items-center text-center">
-        <p className="font-display text-4xl font-semibold text-neutral-950 sm:text-5xl">
-          404
-        </p>
-        <h1 className="mt-4 font-display text-2xl font-semibold text-neutral-950">
-          Page not found
-        </h1>
-        <p className="mt-2 text-sm text-neutral-600">
-          Sorry, we couldn’t find the page you’re looking for.
-        </p>
-        <Link
-          href="/"
-          className="mt-4 text-sm font-semibold text-neutral-950 transition hover:text-neutral-700"
-        >
-          Go to the home page
-        </Link>
-      </FadeIn>
-    </Container>
+    <div className="relative isolate h-full bg-gray-900">
+      <div className="mx-auto grid h-full max-w-7xl grid-cols-1">
+        <div className="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-8 lg:py-48">
+          <div className="mx-auto max-w-xl ">
+            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-white/5">
+              <svg
+                aria-hidden="true"
+                className="absolute inset-0 size-full stroke-gray-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+              >
+                <defs>
+                  <pattern
+                    x="100%"
+                    y={-1}
+                    id="54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2"
+                    width={200}
+                    height={200}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <path d="M130 200V.5M.5 .5H200" fill="none" />
+                  </pattern>
+                </defs>
+                <svg x="100%" y={-1} className="overflow-visible fill-gray-800/20">
+                  <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
+                </svg>
+                <rect fill="url(#54f88622-e7f8-4f1d-aaf9-c2f5e46dd1f2)" width="100%" height="100%" strokeWidth={0} />
+              </svg>
+              <div
+                aria-hidden="true"
+                className="absolute top-[calc(100%-13rem)] -left-56 transform-gpu blur-3xl lg:top-[calc(50%-7rem)] lg:left-[max(-14rem,calc(100%-59rem))]"
+              >
+                <div
+                  style={{
+                    clipPath:
+                      'polygon(74.1% 56.1%, 100% 38.6%, 97.5% 73.3%, 85.5% 100%, 80.7% 98.2%, 72.5% 67.7%, 60.2% 37.8%, 52.4% 32.2%, 47.5% 41.9%, 45.2% 65.8%, 27.5% 23.5%, 0.1% 35.4%, 17.9% 0.1%, 27.6% 23.5%, 76.1% 2.6%, 74.1% 56.1%)',
+                  }}
+                  className="aspect-1155/678 w-[72.1875rem] bg-linear-to-br from-[#80caff] to-[#4f46e5] opacity-20"
+                />
+              </div>
+            </div>
+            <img className="pb-10 justify-self-center" src="sage_horizontal_green_165.png" alt="Logo" />
+
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-center text-white sm:text-5xl">We're upgrading!</h2>
+            <p className="mt-6 text-center text-lg/8 text-gray-300">
+             Our website is currently getting a full remodel! 
+             <br/>
+             The new site will be online shortly.
+            </p>
+            <dl className="mt-10 place-items-center space-y-4 text-base/7 text-gray-300">
+
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Telephone</span>
+                  <PhoneIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                </dt>
+                <dd>
+                  <a href="tel:+1 (503) 454-6438" className="hover:text-white">
+                    +1 (503) 454-6438
+                  </a>
+                </dd>
+              </div>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Email</span>
+                  <EnvelopeIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                </dt>
+                <dd>
+                  <a href="mailto:info@sagetech.support" className="hover:text-white">
+                  info@sagetech.support
+                  </a>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
